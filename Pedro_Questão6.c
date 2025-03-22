1cce0a6b091388083d438baa8a7f95f83a1733c8
Pedroh63 <pedro.brenzink@catolicasc.edu.br>
2025-03-22 12:22:11 -0300

    Update Pedro_Questão6.c

--- Pedro_Questão6.c
+++ Pedro_Questão6.c
@@ -1,18 +1,23 @@
-
 #include <stdio.h>
 
 int main(void)
 {
-   int n1,n2,n3;
+    int n1,n2,n3;
    
-   printf("Indique o primeiro número: ");
-   scanf("%d", &n1);
-   printf("Indique o segundo número: ");
-   scanf("%d", &n2);
-   printf("Indique o terceiro número: ");
-   scanf("%d", &n3);
+    printf("Indique o primeiro número: ");
+    scanf("%d", &n1);
+    printf("Indique o segundo número: ");
+    scanf("%d", &n2);
+    printf("Indique o terceiro número: ");
+    scanf("%d", &n3);
    
-   if (n1 > n2 && n1 > n3)
+    if (n1 == n2 && n2 == n3 || n1 == n2 && n1 > n3 || n1 == n3 && n1 > n2 ||
+    n2 == n3 && n2 > n1)
+    {
+        printf("Os valores são iguais");
+    }
+    
+     if (n1 > n2 && n1 > n3)
    {
        printf("O maior valor é %d", n1);
    }
@@ -26,5 +31,4 @@ int main(void)
     printf("O maior valor é %d", n3);   
    }
    return 0;
-
 }
